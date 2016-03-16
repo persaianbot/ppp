71b53376a530441d6ea2c82c0e7f7b10b5626e3b
persaianbot <mrx2121mrx@gmail.com>
2016-03-16 04:36:58 -0800

    Update myid.lua

--- plugins/myid.lua
+++ plugins/myid.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches ) 
-  if maches[1]:lower() = "id" then
-    return "your id���🏻\n "..msg.from.id
+  if matches[1] == "myid" then
+    return "your id😐👇🏻\n "..msg.from.id
   end
 end
 
 return {
   patterns ={
-    "^[Ii]d$" 
+    "^[/! #](myid)$" 
  }, 
   run = run 
 }
