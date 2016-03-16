ea8b29f6a67eae4e5850c97196f485b59ddc1585
persaianbot <mrx2121mrx@gmail.com>
2016-03-16 05:16:23 -0800

    Update spam.lua

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -1,28 +1,19 @@
-do
+local function run(msg, matches)
 
-function run(msg, matches)
-       if not is_sudo(msg) then
-              return "Only Sudoers Can Use it"       end
-  local tex = matches[1]
-  local sps = matches[2]
-  local sp = 'Sucessfully done'
-
-for i=1, tex, 1 do
-
-sp = '\n'..sps..'\n'..sp
-i = i + 1
-
+  local receiver = get_receiver(msg)
+    if matches[1] == "spam" and is_admin(msg) then
+    local num = matches[2]
+     local text = matches[3]
+        for i=1,num do
+            send_large_msg(receiver, text)
+        end
+  end
 end
-
-return sp
-
-end
-
+ 
 return {
-    patterns = {
-      "spam (.*) (.*)$"
-    },
-    run = run,
+  patterns = {
+  "^[!/](spam) (%d+) (.*)$",
+  "^(spam) (%d+) (.*)$",
+  },
+  run = run,
 }
-
-end
